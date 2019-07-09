ae006fa6742b380f8eaa601b9b7b31761dc8004c
pavlentiysv <pavlentiysv@gmail.com>
2019-07-09 17:01:42 +0300

    small fixes in code style

--- js/08.07/task1.js
+++ js/08.07/task1.js
@@ -1,18 +1,18 @@
 function createObject(obj) {
   for (let [key, value] of Object.entries(obj)) {
     if (obj.hasOwnProperty(key) && typeof value === 'function')
-    obj[key] = value.bind(obj);
+      obj[key] = value.bind(obj);
   }
-	return obj;
+  return obj;
 }
 
 // Expected result
 
 const obj1 = {
-	testField: 1,
-	getTestField: function() {
-		return this.testField;
-	}
+  testField: 1,
+  getTestField: function() {
+    return this.testField;
+  }
 };
 
 const getTestField1 = obj1.getTestField;
@@ -20,10 +20,10 @@ const getTestField1 = obj1.getTestField;
 // getTestField1() // => undefined, context was lost
 
 const obj2 = createObject({
-	testField: 1,
-	getTestField: function() {
-		return this.testField;
-	}
+  testField: 1,
+  getTestField: function() {
+    return this.testField;
+  }
 });
 
 const getTestField2 = obj2.getTestField;
